915c0c8cfc05f437053327de3e22a16040aea000
HenriqueOliveiradaRocha <henrique.oliveira33@hotmail.com.br>
2018-08-23 20:36:37 -0300

    funcoes

---
+++ javaemhtml/funcao.js
@@ -0,0 +1,13 @@
+function funcao() {
+document.getElementById("At1").innerHTML = "Agora esse titulo está correto.";
+}
+function funcao1() {
+	document.getElementById("At2").style.color = "green";
+document.getElementById("At2").style.fontSize = "50px";
+}
+function funcao2() {
+	document.getElementById("At3").style.visibility="hidden";
+}
+function funcao3() {
+	document.getElementById("At3").style.visibility="visible";
+}
