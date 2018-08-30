9763c98ff61285b988ee9d150d53631997c37ad2
HenriqueOliveiradaRocha <henrique.oliveira33@hotmail.com.br>
2018-08-30 20:26:31 -0300

    Teste de função

---
+++ FuncoeseArrays/divisao.js
@@ -0,0 +1,9 @@
+function divisao(x,y) {
+	return x/y;
+}
+var resultado1 = divisao(10,2);
+alert(resultado1);
+	     function linha() {
+			 document.getElementById("l1").style.color="red";
+		 }
+		 // return é o que eu quero mandar da função, se eu coloca no return 1. todos que chamarem essa função receberam 1
