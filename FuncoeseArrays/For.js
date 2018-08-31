f17b999ebcfe275c8afee76a578b12e219ccf94f
HenriqueOliveiradaRocha <henrique.oliveira33@hotmail.com.br>
2018-08-30 22:17:29 -0300

    primeirotesteForemhmlt

---
+++ FuncoeseArrays/For.js
@@ -0,0 +1,9 @@
+function marcas(marcasfamosas) {
+	var p;
+	for(p = 0;p<= marcasfamosas.length;p++){
+		console.log(marcasfamosas[p]);
+	}
+}
+	var marcas1 = ["coca-cola","Doritos","Friboi","Sadia"];
+	marcas(marcas1);
+	console.log("Quantidade de marcas: "+marcas1.length);
