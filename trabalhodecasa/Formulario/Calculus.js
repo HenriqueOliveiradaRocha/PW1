78340b34c95c92b1d796928063f732cfd7cbdc5a
HenriqueOliveiradaRocha <henrique.oliveira33@hotmail.com.br>
2018-09-12 12:57:10 -0300

    Formulario

---
+++ trabalhodecasa/Formulario/Calculus.js
@@ -0,0 +1,10 @@
+function calculus(){
+	var qtd= parseInt(document.getElementById('cquant').value);
+	var tot = qtd*1300;
+	document.getElementById('cvalor').value = tot;
+	var qtd1= parseInt(document.getElementById('cquant1').value);
+	var tot1 = qtd1*1550;
+	document.getElementById('cvalor1').value = tot1;
+	var qtd2= parseInt(document.getElementById('cquant2').value);
+	var tot0 = qtd2*900;
+	document.getElementById('cvalor2').value = tot0;
