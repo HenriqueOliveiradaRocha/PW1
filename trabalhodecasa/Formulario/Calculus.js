59f3087187b7591a4782add2c42a079a635bee57
HenriqueOliveiradaRocha <henrique.oliveira33@hotmail.com.br>
2018-09-13 14:45:01 -0300

    Edicoesnoformulario

--- trabalhodecasa/Formulario/Calculus.js
+++ trabalhodecasa/Formulario/Calculus.js
@@ -1,10 +1,10 @@
 function calculus(){
-	var qtd= parseInt(document.getElementById('cquant').value);
-	var tot = qtd*1300;
-	document.getElementById('cvalor').value = tot;
-	var qtd1= parseInt(document.getElementById('cquant1').value);
+	var qtd= document.getElementById('cquant');
+	tot = qtd*1300;
+	document.getElementById('cvalor') = tot;
+	var qtd1= document.getElementById('cquant1');
 	var tot1 = qtd1*1550;
-	document.getElementById('cvalor1').value = tot1;
-	var qtd2= parseInt(document.getElementById('cquant2').value);
+	document.getElementById('cvalor1') = tot1;
+	var qtd2= document.getElementById('cquant2');
 	var tot0 = qtd2*900;
 	document.getElementById('cvalor2').value = tot0;
