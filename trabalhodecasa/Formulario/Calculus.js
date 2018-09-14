120241ce2fbd7742cad864a39b6315cd07fb8ef5
Henrique Oliveira da Rocha <41965296+HenriqueOliveiradaRocha@users.noreply.github.com>
2018-09-13 21:49:38 -0300

    Formulariopronto

--- trabalhodecasa/Formulario/Calculus.js
+++ trabalhodecasa/Formulario/Calculus.js
@@ -1,10 +1,34 @@
-function calculus(){
-	var qtd= document.getElementById('cquant');
-	tot = qtd*1300;
-	document.getElementById('cvalor') = tot;
-	var qtd1= document.getElementById('cquant1');
-	var tot1 = qtd1*1550;
-	document.getElementById('cvalor1') = tot1;
-	var qtd2= document.getElementById('cquant2');
-	var tot0 = qtd2*900;
-	document.getElementById('cvalor2').value = tot0;
+
+function Valida() {
+	var name= document.getElementById('cnome').value;
+	if (name=="")
+	alert ("Campo NOME não preenchido");
+	validasenha();
+	confirmsenha();
+	email();
+	data();
+}
+
+function validasenha() {
+	var senha= document.getElementById('csenha').value;
+	if (senha=="")
+		alert ("Campo senha vazio");
+}
+
+function confirmsenha() {
+	var confir= document.getElementById('cconf').value;
+	if (confir=="")
+		alert("Campo confime a senha vazio");
+}
+
+function email() {
+	var gmail= document.getElementById('cmail').value;
+	if (gmail=="")
+		alert("Campo E-mail vazio")
+}
+
+function data() {
+	var ddata= document.getElementById('cdata').value;
+	if (ddata=="")
+		alert("Campo Data de nascimento não preenchido")
+}
---
+++ trabalhodecasa/Formulario/Teste.js
@@ -0,0 +1,17 @@
+function Calculus() {
+	var pre= document.getElementById('cquant').value;
+	 var tot= pre*1200;
+	 document.getElementById('cvalor').value = tot;
+}
+
+function Calculus1() {
+	var pre1= document.getElementById('cquant1').value;
+	var tot1= pre1*1550;
+	document.getElementById('cvalor1').value = tot1;
+}
+
+function Calculus2(){
+	var pre2= document.getElementById('cquant2').value;
+	var tot2= pre2*900
+	document.getElementById('cvalor2').value = tot2;
+}
